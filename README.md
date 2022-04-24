0426b238fbc7687a64e7856a00e76638e5e32349
Partha Pritam Paul <partha@Parthas-MacBook-Pro.local>
2022-04-24 23:13:59 +0530

    rate limiter no docker

---
+++ README.md
@@ -0,0 +1,27 @@
+1. Start the docker 
+   docker network create global //creating global network, in docker-container it's not required
+   docker-compose up --build
+   docker container list
+      docker exec -it contaier_name_or_id /bin/bash // to enter container bash of webapp
+      inside bash
+      (1st time only if required) npm i -D package_name // if any package is missing in package.json , it will reflect in both -- -- //-- docker and local sys
+      npm start
+      *now hit apis
+  To test in Redis
+      docker exec -it contaier_name_or_id /bin/bash  //of redis
+      Inside bash
+          redis-cli
+             KEYS *
+  
+
+NOTE: to remove a file "rm -rf dist"
+      to open a file in terminal "cat filename"
+2. In local terminal "npm run dev"
+    And trigger api:: http://localhost:4000/api/ping
+    from both PC and Phone(http://external_ip_of_pc:4000/api/ping)
+    *Phone and PC should be connected with same WiFi
+    *external_ip_of_pc will be available at System Preferences>Network>Click WiFi on left side of Dialog box>IP will be available at right side of the dialog box under status:Connected
+3. To enter redis cli 
+   Go to Docker-Desktop, Click CLI icon of the Redis container
+   use command: "redis-cli"
+   check IP addresses:  "KEYS *"
---
+++ src/limiter.ts
@@ -0,0 +1,54 @@
+const redis = require("redis");
+const RateLimit = require("express-rate-limit");
+const RedisStore = require("rate-limit-redis");
+import { Request, Response } from "express";
+const { MemoryStore } = require("express-rate-limit");
+const requestIp = require("request-ip");
+// import * as redis from 'redis';
+
+// const { endpointUri, password } = require("../config").redis;
+// const redisClient = redis.createClient(`redis://${endpointUri}`, { password });
+const redisClient = redis.createClient({ url: "redis://localhost:6379/" });
+// const redisClient = redis.createClient({ url: "redis://0.0.0.0:6379/" });
+// const redisClient = redis.createClient({ url: "redis://redis:6379" });
+// const redisClient = redis.createClient(process.env.REDIS_URL);
+// const redisClient = redis.createClient({ url: "redis://redis:6379/" });
+// const redisClient = redis.createClient();
+// const redisClient = redis.createClient(6379, 'redis');
+// const redisClient = redis.createClient({
+//   host: "redis",
+//   port: 6379,
+// });
+// const redisClient = redis.createClient({host: 'redis'});
+
+// const redisClient = redis.createClient();
+
+async function limiterCall() {
+  const limiter = RateLimit({
+    // Rate limiter configuration
+    max: 10, //10 req
+    windowMs: 1 * 60 * 1000, //1 min in ms
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    // headers: true,
+    // keyGenerator: (req: Request, res: Response) => {
+    //   return requestIp.getClientIp(req); // Client IP address from requestIp.mw(), as opposed to req.ip
+    //   // limiter.store.sendCommand(requestIp.getClientIp(req));
+    //   // return req.clientIp;
+    // },
+
+    // Redis store configuration
+    // store: new MemoryStore(),
+    store: new RedisStore({
+      client: redisClient,
+      expiry: 1 * 60, // 1 min in seconds
+      sendCommand: (...args: string[]) => redisClient.sendCommand(args),
+    }),
+  });
+  module.exports = limiter;
+}
+
+redisClient
+  .connect()
+  .then(limiterCall())
+  .catch((e: Error) => console.log(e));
